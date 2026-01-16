14dee78fcbe8a290596a547b7b6a825a7151745e
sindhu hegde <sindhuhegde2007@gmail.com>
2026-01-16 19:26:25 +0530

    Update test_fahrenheit.py

--- test_fahrenheit.py
+++ test_fahrenheit.py
@@ -1,17 +1,25 @@
-from fahrenheit import celsius_to_fahrenheit
+from fahrenheit import classify_temperature
 
-def test_zero_celsius():
-    result = celsius_to_fahrenheit(0)
-    print("0C ->", result)
-    assert result == 32
 
-def test_boiling_point():
-    result = celsius_to_fahrenheit(100)
-    print("100C ->", result)
-    assert result == 212
+def test_temperature_case1():
+    expected_output = (
+        "Temperature:10\n"
+        "Status:Cold"
+    )
+    assert classify_temperature(10) == expected_output
 
-def test_negative_temperature():
-    result = celsius_to_fahrenheit(-40)
-    print("-40C ->", result)
-    assert result == -40
 
+def test_temperature_case2():
+    expected_output = (
+        "Temperature:25\n"
+        "Status:Normal"
+    )
+    assert classify_temperature(25) == expected_output
+
+
+def test_temperature_case3():
+    expected_output = (
+        "Temperature:35\n"
+        "Status:Hot"
+    )
+    assert classify_temperature(35) == expected_output
